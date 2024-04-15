53da2e62c6633860e44910dc14a120237c250784
Nicolás Baigorria <nicobaig@hotmail.com>
2024-04-14 23:46:13 -0300

    agregar nombre a usuarios

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
- return <div>
-    <button>Mis Libros</button>
-    <button>Desconectar</button>
- </div>
+  const navigate = useNavigate();
+
+  const handleMisLibrosClick = () => {
+    navigate("/books");
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  return (
+      <div className="container mt-4">
+      <h1 className="text-center mb-4">Books App</h1> 
+      <div className="d-flex justify-content-center"> 
+        <button className="btn btn-primary me-3" onClick={handleMisLibrosClick}>Mis Libros</button>
+        <button className="btn btn-danger" onClick={handleLogout}>Desconectar</button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -26,38 +26,50 @@ const LoginPage = () => {
   };
 
   return (
-    <form onSubmit={handleSubmitEvent}>
-      <div className="form_control">
-        <label htmlFor="user-email">Email:</label>
-        <input
-          type="email"
-          id="user-email"
-          name="username"
-          placeholder="example@yahoo.com"
-          aria-describedby="user-email"
-          aria-invalid="false"
-          onChange={handleInput}
-        />
-        <div id="user-email" className="sr-only">
-          Please enter a valid username. It must contain at least 6 characters.
+    <div className="container mt-4">
+      <h2 className="text-center mb-4">Iniciar Sesión</h2>
+      <form onSubmit={handleSubmitEvent} className="container mt-4">
+        <div className="mb-3">
+          <label htmlFor="user-email" className="form-label">
+            Email:
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="user-email"
+            name="username"
+            placeholder="example@yahoo.com"
+            aria-describedby="user-email"
+            aria-invalid="false"
+            onChange={handleInput}
+          />
+          <div id="user-email" className="form-text">
+            Please enter a valid username. It must contain at least 6
+            characters.
+          </div>
         </div>
-      </div>
-      <div className="form_control">
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          aria-describedby="user-password"
-          aria-invalid="false"
-          onChange={handleInput}
-        />
-        <div id="user-password" className="sr-only">
-          your password should be more than 6 character
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            aria-describedby="user-password"
+            aria-invalid="false"
+            onChange={handleInput}
+          />
+          <div id="user-password" className="form-text">
+            Your password should be more than 6 characters.
+          </div>
         </div>
-      </div>
-      <button className="btn-submit">Login</button>
-    </form>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
+    </div>
   );
 };
 
